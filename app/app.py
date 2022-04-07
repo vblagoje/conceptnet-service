2f1027c9506a939eb8445b790116bea4664e9fc9
Vladimir Blagojevic <dovlex@gmail.com>
2022-04-07 17:59:02 +0200

    Add automatic graph data download

--- app/app.py
+++ app/app.py
@@ -1,12 +1,30 @@
-from typing import Dict, Any
+import os
+from pathlib import Path
 
 from fastapi import FastAPI, Request
+
 from .graph_resolver import GraphResolver
+from .util import download
+
+cpnet_vocab_path = {"path": "./app/data/concept.txt", "url": "https://storage.googleapis.com/hf_datasets/concept.txt"}
+pattern_path = {"path": "./app/data/matcher_patterns.json",
+                "url": "https://storage.googleapis.com/hf_datasets/matcher_patterns.json"}
+pruned_graph_path = {"path": "./app/data/conceptnet.en.pruned.graph",
+                     "url": "https://storage.googleapis.com/hf_datasets/conceptnet.en.pruned.graph"}
 
 app = FastAPI()
-graph_resolver = GraphResolver(cpnet_vocab_path="./app/data/concept.txt",
-                               pattern_path="./app/data/matcher_patterns.json",
-                               pruned_graph_path="./app/data/conceptnet.en.pruned.graph")
+
+required_data = [cpnet_vocab_path, pattern_path, pruned_graph_path]
+
+for file in required_data:
+    if not os.path.exists(file['path']):
+        parent_dir = Path(file['path']).parent.absolute()
+        os.makedirs(str(parent_dir), exist_ok=True)
+        download(file['url'], file['path'])
+
+graph_resolver = GraphResolver(cpnet_vocab_path=cpnet_vocab_path['path'],
+                               pattern_path=pattern_path['path'],
+                               pruned_graph_path=pruned_graph_path['path'])
 
 
 @app.post("/csqa/")
@@ -16,6 +34,7 @@ async def resolve_csqa(request: Request):
     statement = graph_resolver.convert_qajson_to_entailment(qa_json=common_sense_qa_example, ans_pos=False)
     grouned_statements = graph_resolver.ground(statement)
     result = graph_resolver.generate_adj_data_from_grounded_concepts__use_lm(statement, grouned_statements)
+
 
 @app.on_event("startup")
 def on_startup():
